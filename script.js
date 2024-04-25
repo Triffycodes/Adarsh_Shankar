5027689d4768a908bcb10b00b71af6f98746f71a
Triffycodes <adarsh.s.gowda20@gmail.com>
2024-04-25 15:51:21 -0500

    change experience and coding profile section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
@@ -23,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+// PROJECTS SECTION
 
 //step 1: get DOM
 let nextDom = document.getElementById('next');
@@ -74,6 +77,8 @@ function showSlider(type){
     }, timeAutoNext)
 }
 
+// CODING PROFILE SECTION
+
 // Get the container and links
 const platformLinks = document.querySelectorAll('.platform-link');
 const dashboardContainer = document.querySelector('.dashboard-container');
@@ -85,8 +90,22 @@ platformLinks.forEach((link) => {
     e.preventDefault();
     const platform = link.getAttribute('data-platform');
     const username = link.getAttribute('data-username');
+    const domainName = link.getAttribute('data-domain');
+    
     // Use the platform name to construct the dashboard URL
-    const dashboardUrl = `https://${platform}.com/${username}`;
+    let dashboardUrl;
+    if (platform === 'github'){   
+      dashboardUrl = 'https://github.com/Triffycodes';
+    } 
+    else if(platform === 'hackerrank'){
+      dashboardUrl = 'https://www.hackerrank.com/profile/TriffyCodes';
+    }
+    else if(platform === 'gfg'){
+      dashboardUrl = 'https://www.geeksforgeeks.org/user/triffycodes/';
+    }else {
+      dashboardUrl = `https://www.${platform}.${domainName}/${username}`;
+    }
+    
     // Create an iframe to display the dashboard
     const iframe = document.createElement('iframe');
     iframeContainer.classList.add('iframe-container');
@@ -95,16 +114,53 @@ platformLinks.forEach((link) => {
     iframe.width = '100%';
     iframe.height = '100%';
     iframe.setAttribute('style', 'overflow: hidden; width: 100%; height: 70vh;');
-    // Add the iframe container class to the iframe container element
     
     // Add the iframe to the dashboard container
     dashboardContainer.innerHTML = '';
+    const buttonContainer = document.createElement('div');
+    buttonContainer.style.display = 'flex';
+    buttonContainer.style.justifyContent = 'center';
+    buttonContainer.style.marginBottom = '20px';
+    dashboardContainer.appendChild(buttonContainer);
     dashboardContainer.appendChild(iframe);
 
     // Show the dashboard container
     dashboardContainer.style.display = 'block';
-
+    
     // Add the expanded class to the iframe container
     iframeContainer.classList.add('expanded');
+    
+    // If GitHub, add a message and button
+    if (platform === 'github' || platform === 'hackerrank' || platform === 'gfg') {
+      const message = document.createElement('p');
+      message.textContent = "Ohh snap!, they don't want me to show my progress :( click here!";
+      buttonContainer.appendChild(message);
+      const br = document.createElement('br'); // Add a line break
+      buttonContainer.appendChild(br);
+      const button = document.createElement('button');
+      button.textContent = 'Visit Site';
+      button.style.background = 'linear-gradient(to right, #ff69b4, #ff97c9)';
+      button.style.marginLeft = '1rem';
+      button.style.color = '#fff';
+      button.style.padding = '10px 20px';
+      button.style.border = 'none';
+      button.style.borderRadius = '10px';
+      button.style.boxShadow = '0px 0px 10px rgba(0,0,0,0.5)';
+      button.style.cursor = 'pointer';
+      buttonContainer.appendChild(button);
+      button.style.transition = 'transform 0.5s';
+      button.addEventListener('mouseover', () => {
+        button.style.transform = 'scale(1.3)';
+      });
+      
+      button.addEventListener('mouseout', () => {
+        button.style.transform = 'scale(1)';
+      });
+      
+      
+      button.addEventListener('click', () => {
+        window.location.href = dashboardUrl;
+      });
+    }
   });
 });
